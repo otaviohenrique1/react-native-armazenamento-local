db793b69af0e2d6c676ab779e7ec35bfff04af32
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-08-03 18:36:10 -0300

    commit 1

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import NotaEditor from "./src/componentes/NotaEditor";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={estilos.container}>
+      <NotaEditor />
+      <StatusBar />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "stretch",
+    justifyContent: "flex-start",
   },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-native-armazenamento-local
+
+Projeto feito no curso "React Native: guardando informações localmente" da Alura
