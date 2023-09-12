6c1e0b589786150e7ae98810814cd494d9813ae0
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-09-12 15:37:51 -0300

    commit

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { FlatList, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
 import NotaEditor from "./src/componentes/NotaEditor";
 // import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
